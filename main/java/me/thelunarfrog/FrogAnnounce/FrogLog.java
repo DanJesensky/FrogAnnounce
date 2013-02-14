571360a663181e69f64723a415460a4b44ef2124
Dan <theexplosivefrog@aim.com>
2013-02-14 15:35:35 -0500

    Strip ugly characters from console messages.
    Those shouldn't be there anyways.

--- main/java/me/thelunarfrog/FrogAnnounce/FrogLog.java
+++ main/java/me/thelunarfrog/FrogAnnounce/FrogLog.java
@@ -7,12 +7,12 @@ import org.bukkit.ChatColor;
 
 public class FrogLog{
 	protected void info(String i){
-		Logger.getLogger("Minecraft").log(Level.INFO, ChatColor.DARK_GREEN+"[FrogAnnounce] "+ChatColor.GREEN+i);
+		Logger.getLogger("Minecraft").log(Level.INFO, "[FrogAnnounce] "+ChatColor.stripColor(i));
 	}
 	protected void warning(String w){
-		Logger.getLogger("Minecraft").log(Level.WARNING, ChatColor.DARK_GREEN+"[FrogAnnounce] "+ChatColor.RED+w);
+		Logger.getLogger("Minecraft").log(Level.WARNING, "[FrogAnnounce] "+ChatColor.stripColor(w));
 	}
 	protected void severe(String s){
-		Logger.getLogger("Minecraft").log(Level.SEVERE, ChatColor.DARK_GREEN+"[FrogAnnounce] "+ChatColor.DARK_RED+s);
+		Logger.getLogger("Minecraft").log(Level.SEVERE, "[FrogAnnounce] "+ChatColor.stripColor(s));
 	}
 }
